0b2a9e17b6308866fe6b0b7a5f7b49ac9c40a065
Leslie Rodriguez <leslie286@usf.edu>
2024-10-20 22:34:40 -0500

    Handle stock availability and disable purchase button when out of stock

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ sizeDropdown.addEventListener {
 const selectedSize ();
 const updatedPrice ();
 }
+
+// Task 3: Handle Stock Availability
+const pricemap = {
+    small: 20
+    medium: 30
+    large: 40
+};
+
+const sizeDropdown = .getElementById('size-options');
+}const sizeDropdown = .getElementById('product price');
+if ( 'out-of-stock') {
+    purchaseButton.disabled = true;
+    purchaseButton.Content = 'Out of Stock';
